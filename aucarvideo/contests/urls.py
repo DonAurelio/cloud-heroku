4953adbfa984c4cc5e61c46277dd7a4a137f0e5d
juanca0517 <jc.cifuentes@uniandes.edu.co>
2018-08-26 00:40:33 -0500

    insertar y eliminar concurso

--- aucarvideo/contests/urls.py
+++ aucarvideo/contests/urls.py
@@ -1,9 +1,14 @@
 # -- encoding: utf-8 --
 
 from django.urls import path
+from django.conf.urls import url
 
-from contests.views import NewContest
+
+from contests.views import (
+    NewContest,
+    ContestDelete)
 
 urlpatterns = [
     path('videocontest_form/', NewContest.as_view(), name='videocontest_form'),
+    url(r'^videocontest_confirm_delete/(?P<pk>\d+)$',ContestDelete.as_view(),name='videocontest_confirm_delete'),
 ]
