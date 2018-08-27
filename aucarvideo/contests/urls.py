c338c907150cf4d9762ad4cabb8680ba926a1ed2
juanca0517 <jc.cifuentes@uniandes.edu.co>
2018-08-26 22:48:55 -0500

    Configuracion de view, urls e incorporacion de templates para edicion y detalle de contest

--- aucarvideo/contests/urls.py
+++ aucarvideo/contests/urls.py
@@ -6,9 +6,14 @@ from django.conf.urls import url
 
 from contests.views import (
     NewContest,
-    ContestDelete)
+    ContestDelete,
+    ContestUpdate,
+    VideoList)
 
 urlpatterns = [
-    path('videocontest_form/', NewContest.as_view(), name='videocontest_form'),
+    url(r'^videocontest_form$', NewContest.as_view(), name='videocontest_form'),
+    url(r'^editarContest/(?P<pk>\d+)$', ContestUpdate.as_view(), name='editContest'),
     url(r'^videocontest_confirm_delete/(?P<pk>\d+)$',ContestDelete.as_view(),name='videocontest_confirm_delete'),
+    url(r'^video_list/(?P<contest_id>\d+)$', VideoList.as_view(), name='video_list')
+
 ]
--- aucarvideo/contests/views.py
+++ aucarvideo/contests/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
-from contests.models import VideoContest
+from contests.models import VideoContest, Video
 from django.shortcuts import render, redirect, reverse
+from django.views.generic.edit import UpdateView
+from django.views.generic.base import TemplateView
 
 class NewContest(CreateView):
     model = VideoContest
@@ -17,4 +19,16 @@ class ContestDelete(DeleteView):
     success_url = reverse_lazy('thome:thome')
 
 
+class ContestUpdate(UpdateView):
+    model = VideoContest
+    fields = ['name','banner','url','url','start_date','end_date','award_description']
 
+    def get_success_url(self):
+        return reverse('thome:thome')
+
+
+
+class VideoList(TemplateView):
+    def get(self, request, contest_id):
+        videos = Video.objects.filter(contest_id=contest_id)
+        return render(request, 'contests/video_list.html', {'videos': videos})
