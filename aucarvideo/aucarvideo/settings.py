4da289145046a30a8384c34e3b455c7b0e093e64
CarolinaPardoFuquen <c.pardo@uniandes.edu.co>
2018-09-22 21:50:59 -0500

    prueba para produccion en Amazon

--- aucarvideo/aucarvideo/settings.py
+++ aucarvideo/aucarvideo/settings.py
@@ -177,13 +177,14 @@ WSGI_APPLICATION = 'aucarvideo.wsgi.application'
 DATABASES = {
     'default': {
     'ENGINE': 'tenant_schemas.postgresql_backend',
+    #'NAME': os.environ.get('DB_NAME' if PRODUCTION else 'POSTGRES_DB'),
     'NAME': os.environ.get('DB_NAME'),
     'USER': os.environ.get('DB_USER'),
     'PASSWORD': os.environ.get('DB_PASS'),
     # If your are in development put in '' the IP addres
     # of the postgres server
-    'HOST': os.environ.get('DB_SERVICE' if PRODUCTION else 'DB_HOST') ,
-    'PORT': os.environ.get('DB_PORT','5432')
+    'HOST': os.environ.get('DB_HOST') ,
+    'PORT': os.environ.get('DB_PORT')
     }
 }
 
