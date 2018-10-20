3e395c5f2ff19bbce71bfbe3d5735bda980c7dee
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2018-10-20 14:50:41 -0500

    removing django tenants from requirements

--- web/aucarvideo/entrypoint.sh
+++ web/aucarvideo/entrypoint.sh
@@ -7,15 +7,10 @@ source secrets.sh
 
 ./manage.py makemigrations --noinput
 
-./manage.py migrate_schemas --noinput
-
-./manage.py migrate_schemas --shared --noinput
+./manage.py migrate --noinput
 
 # colecting staticfiles in sstatic dir
 ./manage.py collectstatic --noinput
-
-# Creating the public domain to acces the application
-./manage.py create_public_tenant
 
 # Running the django application
 gunicorn aucarvideo.wsgi --timeout 600 -b 0.0.0.0:80
