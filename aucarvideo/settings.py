981479f075496f461944005e1c84691db098eff0
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2018-11-03 20:54:00 -0500

    Using heroku memcachier

--- aucarvideo/settings.py
+++ aucarvideo/settings.py
@@ -199,9 +199,54 @@ ELB_URL_FORMAT = ELB_BASE_URL + '{path}'
 PAGINATION_BY = int(os.environ.get('PAGINATION_BY','50'))
 
 # Memcached
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': 'aucarvideod-mc.htrxbs.cfg.usw2.cache.amazonaws.com:11211',
+#     }
+# }
+
+servers = os.environ['MEMCACHIER_SERVERS']
+username = os.environ['MEMCACHIER_USERNAME']
+password = os.environ['MEMCACHIER_PASSWORD']
+
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': 'aucarvideod-mc.htrxbs.cfg.usw2.cache.amazonaws.com:11211',
+        # Use pylibmc
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+
+        'LOCATION': servers,
+
+        'OPTIONS': {
+            # Use binary memcache protocol (needed for authentication)
+            'binary': True,
+            'username': username,
+            'password': password,
+            'behaviors': {
+                # Enable faster IO
+                'no_block': True,
+                'tcp_nodelay': True,
+
+                # Keep connection alive
+                'tcp_keepalive': True,
+
+                # Timeout settings
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
+
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30,
+            }
+        }
     }
 }
